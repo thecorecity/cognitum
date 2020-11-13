aec5b3ac16429b94cec507ded46504541a83074a
KoloMl <thekolomoets@hotmail.com>
2020-11-13 19:53:40 +0400

    Autoformatting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Bot = require("./lib/classes/Bot.js");
-const {log} = require("./lib/classes/Utils.js");
+const { log } = require("./lib/classes/Utils.js");
 
 const cognitum = new Bot();
 cognitum.initialize().then(() => {
