ff36d766b23a2c055bf54fe408a02ed5763c2e2f
KoloMl <thekolomoets@hotmail.com>
2020-11-18 21:10:08 +0400

    Moving sharding manager initialization to separated class

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const { ShardingManager } = require("discord.js");
-const ConfigManager = require("./lib/classes/ConfigManager");
+const CognitumShardingManager = require("./lib/classes/CognitumShardingManager");
+const { log } = require("./lib/classes/Utils");
 
-ConfigManager.initialize().then(() => {
-	const shardingManager = new ShardingManager("./shard.js", { token: ConfigManager.get("auth.discord.token") });
-	shardingManager.on("shardCreate", shard => console.log(`Loaded shard #${shard.id}!`));
-	return shardingManager.spawn();
-}).then(() => {
-	console.log("Spawning successful!");
-}).catch((error) => {
-	console.error("Failed to initialize ShardingManager!");
-	console.error(error);
-	process.exit();
+CognitumShardingManager.initialize().then(() => {
+	log("success", "Initialization successful!");
 });
