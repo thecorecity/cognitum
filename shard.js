b78a21eabf55e4cb309ebfb4b41841be2020ed7f
KoloMl <thekolomoets@hotmail.com>
2020-11-29 23:47:23 +0400

    Passing intents into Client constructor

--- shard.js
+++ shard.js
@@ -1,7 +1,17 @@
-const CognitumClient = require("./lib/classes/CognitumClient.js");
-const { log } = require("./lib/classes/Utils.js");
+const CognitumClient = require("./lib/classes/CognitumClient");
+const { log } = require("./lib/classes/Utils");
+const { Intents } = require("discord.js");
 
-const cognitum = new CognitumClient();
+const cognitum = new CognitumClient({
+	disableMentions: "everyone",
+	ws: {
+		// TODO Specify all intents required for bot functionality
+		intents: new Intents([
+			Intents.NON_PRIVILEGED,
+			Intents.FLAGS.GUILD_MEMBERS
+		])
+	}
+});
 cognitum.initialize().then(() => {
 	log("success", "Initialization success!");
 }).catch(error => {
