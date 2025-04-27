cf594e21e5e417a5f4fc75b420d17f8fb339c7b8
KoloMl <thekolomoets@hotmail.com>
2025-04-28 01:08:00 +0400

    Converting base model class to the TS
    I have an issue with this one, but it should work for now. This class
    requires refactoring.

--- lib/classes/base/BaseModel.d.ts
+++
@@ -1,8 +0,0 @@
-import {InferAttributes, InferCreationAttributes, InitOptions, Model, ModelAttributes, Sequelize} from "sequelize";
-
-export default abstract class BaseModel<TSelf extends Model> extends Model<InferAttributes<TSelf>, InferCreationAttributes<TSelf>> {
-	static initialize(sequelize: Sequelize);
-
-	protected abstract static attributes: ModelAttributes<TSelf>;
-	protected abstract static initOptions: InitOptions;
-}
---
+++ lib/classes/base/BaseModel.ts
@@ -0,0 +1,24 @@
+import _ from "lodash";
+import {InferAttributes, InferCreationAttributes, InitOptions, Model, ModelAttributes, Sequelize} from "sequelize";
+import {createModuleLogger} from "../Utils";
+
+const logger = createModuleLogger("database");
+
+export default abstract class BaseModel<TSelf extends Model> extends Model<InferAttributes<TSelf>, InferCreationAttributes<TSelf>> {
+	/**
+	 * @inheritDoc
+	 * @abstract
+	 */
+	protected static attributes: ModelAttributes;
+	/**
+	 * @inheritDoc
+	 * @abstract
+	 */
+	static initOptions: InitOptions;
+
+	static initialize(sequelize: Sequelize): void {
+		// FIXME Rework initialization of models to make it more TypeScript-friendly.
+		(this as any).init(this.attributes, _.merge(this.initOptions, { sequelize }));
+		logger.debug(`Database model loaded: ${this.initOptions.tableName}.`);
+	}
+}
